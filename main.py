bdc6bc0126caed12649f2206fe40d3afc4531ebd
Evan <emd5310@g.rit.edu>
2020-02-08 12:35:31 -0500

    Initial commit after pulling onto my machine. Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# The main file of our BrickHack 6 project
+
+
+# Launching the program
+def main():
+    print("Project Starting!")
+    
+
+# Obligatory if-guard
+if __name__ == '__main__':
+    main()
