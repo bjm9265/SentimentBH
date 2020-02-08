8157776a19a85f17df50ce6dab95aa7698d46a50
Evan <emd5310@g.rit.edu>
2020-02-08 14:23:31 -0500

    Credential loader finished

--- credential_loader.py
+++ credential_loader.py
@@ -1,23 +1,29 @@
-
-
+# Takes all the credential information from a txt file, keeping those secure
 def load_creds():
     creds = []
-    with open("credentials.txt") as file:
+    with open("credentials") as file:
         for line in file:
             creds.append(line)
 
     global endpoint
-    endpoint = creds[0]
+    endpoint = creds[0].strip("\n")
     global acs_key
-    acs_key = creds[1]
+    acs_key = creds[1].strip("\n")
     global tw_act
-    tw_act = creds[3]
+    tw_act = creds[3].strip("\n")
     global tw_acs
-    tw_acs = creds[4]
+    tw_acs = creds[4].strip("\n")
     global tw_ck
-    tw_ck = creds[5]
+    tw_ck = creds[5].strip("\n")
     global tw_cs
-    tw_cs = creds[6]
+    tw_cs = creds[6].strip("\n")
 
 
-def get_
+# Dictionary with TW-API creds
+def get_twitter_creds():
+    return {"tw_act": tw_act, "tw_acs": tw_acs, "tw_ck": tw_ck, "tw_cs": tw_cs}
+
+# Dictionary with ACS creds
+def get_acs_creds():
+    return {"endpoint": endpoint, "key": acs_key}
+
--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 The main file of our BrickHack 6 project
 Team Members: Brian Mirabito, Evan Donohue, Sangu Mbekelu
 """
-
+import credential_loader
 
 # Launching the program
 def main():
     print("Project Starting!")
+    credential_loader.load_creds()
+    print(credential_loader.get_acs_creds())
+
 
 
 # Obligatory if-guard
