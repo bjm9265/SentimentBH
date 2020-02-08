7c33163d2f525f9e039c7676b64a756e96807df3
Brian Mirabito <bjm9265@rit.edu>
2020-02-08 14:43:49 -0500

    GetData class that uses our twitter access tokens and keys to get tweets using tweepy.

--- GetData.py
+++ GetData.py
@@ -1,13 +1,31 @@
 import tweepy
+import credential_loader
 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+credential_loader.load_creds()
+credentials = credential_loader.get_twitter_creds()
+
+consumer_key = credentials["tw_ck"]
+consumer_secret = credentials["tw_cs"]
+access_token = credentials["tw_act"]
+access_token_secret = credentials["tw_acs"]
 
 authInfo = tweepy.OAuthHandler(consumer_key, consumer_secret)
 authInfo.set_access_token(access_token, access_token_secret)
 
 twapi = tweepy.API(authInfo)
 
-'tweets = twapi.search("")'
+def set_keys(credentials):
+    global consumer_key
+    consumer_key = credentials["tw_ck"]
+    global consumer_secret
+    consumer_secret = credentials["tw_cs"]
+    global access_token
+    access_token = credentials["tw_act"]
+    global access_token_secret
+    access_token_secret = credentials["tw_acs"]
+
+def hashtag_pull(hash, amount):
+    for i in range(amount):
+        tweets = twapi.search(hash)
+
+    return tweets
